8cb08c74a5c7ae2e9200ee319ba87d9dae31bfeb
Advait Namboodiri <nambooadi@gmail.com>
2026-03-09 16:51:45 -0500

    feat: Supporting years 2023 - 2025 in session fetching

--- racinator/app/lib/openf1.ts
+++ racinator/app/lib/openf1.ts
@@ -1,5 +1,5 @@
-export async function getSessions() {
-    const res = await fetch("https://api.openf1.org/v1/sessions?year=2025", {
+export async function getSessions(year: string) {
+    const res = await fetch(`https://api.openf1.org/v1/sessions?year=${year}`, {
       cache: "no-store",
     });
     if (!res.ok) {
