dbf66366019cb85de049e9ed83cde20146687db1
Advait Namboodiri <nambooadi@gmail.com>
2026-03-09 16:34:36 -0500

    FIX: Added check if response is ok before parsing JSON

--- racinator/app/lib/openf1.ts
+++ racinator/app/lib/openf1.ts
@@ -2,5 +2,8 @@ export async function getSessions() {
     const res = await fetch("https://api.openf1.org/v1/sessions?year=2025", {
       cache: "no-store",
     });
+    if (!res.ok) {
+        throw new Error("Failed to fetch sessions");
+    }
     return res.json();
 }  
