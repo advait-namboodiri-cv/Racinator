e297c8684a05ce42195448b6609bc9802b2ba5fe
Advait Namboodiri <nambooadi@gmail.com>
2026-03-09 16:31:25 -0500

    feat: Added function to read response from API and decode it into text.

--- racinator/datapipeline/fetch_sessions.py
+++ racinator/datapipeline/fetch_sessions.py
@@ -2,3 +2,8 @@ import json
 import urllib.request
 
 URL = "https://api.openf1.org/v1/sessions?year=2025"
+def fetch_data(url: str):
+    with urllib.request.urlopen(url) as response:
+        data = json.loads(response.read().decode("utf-8"))
+    return data
+
